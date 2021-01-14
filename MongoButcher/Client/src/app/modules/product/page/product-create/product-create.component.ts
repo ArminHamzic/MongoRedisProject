e48247555c1a8bc615d8453be720a5b4e9668da8
Christian Bachl <c.bachl@students.htl-leonding.ac.at>
2021-01-14 20:21:56 +0100

    Add create product page

--- MongoButcher/Client/src/app/modules/product/page/product-create/product-create.component.ts
+++ MongoButcher/Client/src/app/modules/product/page/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../../../../data/product';
+import {Category} from '../../../../data/category';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product = new Product();
+
+  categories = Array<Category>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onCreateNewCategory(): void {
+
+  }
+
+  onClose() {
+
+  }
+
+  onSubmit() {
+
+  }
 }
