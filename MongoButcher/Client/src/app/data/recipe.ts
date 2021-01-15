6eff44b62047a87aa3fec973b7da7c38e19928a6
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-01-15 15:55:14 +0100

    adding recipe detail page

--- MongoButcher/Client/src/app/data/recipe.ts
+++ MongoButcher/Client/src/app/data/recipe.ts
@@ -11,8 +11,8 @@ export class Recipe {
   constructor(id?: string, name?: string, procedure?: string, output?: Product, ingredients?: Array<Resource>) {
     this.id = id;
     this.name = name;
-    this.name = name;
     this.procedure = procedure;
     this.resources = ingredients;
+    this.output = output;
   }
 }
