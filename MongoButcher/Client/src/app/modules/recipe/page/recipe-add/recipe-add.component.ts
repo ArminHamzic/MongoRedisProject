d3540a6056e866d70d5e67ecc1e32e6aa0d4ebc9
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-01-15 14:21:46 +0100

    adding ressource table in add recipe

--- MongoButcher/Client/src/app/modules/recipe/page/recipe-add/recipe-add.component.ts
+++ MongoButcher/Client/src/app/modules/recipe/page/recipe-add/recipe-add.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Recipe} from '../../../../data/recipe';
+import {MatTableDataSource} from '@angular/material/table';
+import {Product} from '../../../../data/product';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {ProductService} from '../../../../core/http/product.service';
 
 @Component({
   selector: 'app-recipe-add',
@@ -8,13 +13,38 @@ import {Recipe} from '../../../../data/recipe';
 })
 export class RecipeAddComponent implements OnInit {
 
-  constructor() {
+  displayedColumns: string[] = ['name', 'category', 'amount', 'details', 'search'];
+  dataSource!: MatTableDataSource<Product>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private productService: ProductService) {
     this.recipe = new Recipe();
+
+    this.productService.$products.subscribe((products) => {
+      this.dataSource = new MatTableDataSource(products);
+    });
   }
 
   recipe: Recipe;
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  applyCategoryFilter(filter?: string): void {
+    this.productService.loadProducts(filter);
   }
 
 }
