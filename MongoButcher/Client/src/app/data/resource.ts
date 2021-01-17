356665b42079888939ff9b8ccce9414ac7e64b89
Alex <walliser.alexander@gmail.com>
2021-01-17 19:43:13 +0100

    change actionHistory.ts

--- MongoButcher/Client/src/app/data/resource.ts
+++ MongoButcher/Client/src/app/data/resource.ts
@@ -1,13 +1,16 @@
 import {Product} from './product';
+import {ActionHistory} from './actionHistory';
 
 export class Resource {
   id ?: string;
   product ?: Product;
   amount ?: number;
+  actionHistories ?: Array<ActionHistory>;
 
   constructor(id?: string, product?: Product, amount ?: number) {
     this.id = id;
     this.product = product;
     this.amount = amount;
+    this.actionHistories = [];
   }
 }
