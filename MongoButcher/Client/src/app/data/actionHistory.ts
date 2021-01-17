be826bc08377d3bb821245e19506be3c6bd10281
Alex <walliser.alexander@gmail.com>
2021-01-17 20:08:45 +0100

    change actionHistory.ts

--- MongoButcher/Client/src/app/data/actionHistory.ts
+++ MongoButcher/Client/src/app/data/actionHistory.ts
@@ -3,9 +3,9 @@ import {Product} from './product';
 export class ActionHistory {
   id ?: string;
   description ?: string;
-  creationDate ?: Date;
+  creationDate ?: string;
 
-  constructor(id?: string, description?: string, creationDate?: Date) {
+  constructor(id?: string, description?: string, creationDate?: string) {
     this.id = id;
     this.description = description;
     this.creationDate = creationDate;
